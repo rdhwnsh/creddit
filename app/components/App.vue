af70f665d0c4db366ead63690b34309dad1e850a
Nabil Ridhwanshah <35335428+bynabil@users.noreply.github.com>
2018-10-06 10:25:56 +0800

    Initial commit

---
+++ app/components/App.vue
@@ -0,0 +1,117 @@
+<template>
+    <Page>
+        <ActionBar :title="apptitle" />
+
+        <Scrollview>
+
+            <StackLayout>
+                <!-- <Label class="message"> Hey there </Label> -->
+                <TextField v-model="subreddit" hint="Enter a subreddit" @input="getSubreddit"></TextField>
+                <button @tap="getSubreddit">Get subreddit list</button>
+                <button @tap="coolbutton">{{getnextpost}}</button>
+
+                <!-- <Label class="message bold" textWrap="true">Displaying posts from {{subreddit}} </Label> -->
+
+
+                <Label class="message bold" textWrap="true">{{title}} </Label>
+                <Image :src="img" />
+                <Label class="message" textWrap="true">{{usernameDisplay}} </Label>
+
+            </StackLayout>
+
+            <!-- <GridLayout colums="*" rows="*">
+            <Label class="message" :text="usernameDisplay" col="0" row="0"/>
+        </GridLayout> -->
+
+
+        </Scrollview>
+    </Page>
+</template>
+
+<script>
+    var Toast = require("nativescript-toast")
+    export default {
+        data() {
+            return {
+                usernameDisplay: "",
+                taptimes: 0,
+                apptitle: "dev.bynabil.testingapp",
+                data: [],
+                subreddit: "todayilearned",
+                title: "",
+                img: "",
+                getnextpost: "Press the button above first!"
+            }
+        },
+        methods: {
+            coolbutton() {
+                this.taptimes++;
+                this.usernameDisplay = "/u/" + this.data[this.taptimes].data.author
+                this.title = this.data[this.taptimes].data.title
+                this.img = this.data[this.taptimes].data.thumbnail
+
+                if (this.taptimes >= 24) {
+                    this.taptimes = 0;
+                }
+
+            },
+            getSubreddit() {
+                fetch("https://www.reddit.com/r/" + this.subreddit + ".json")
+                    .then(response => response.json())
+                    .then(json => {
+                        this.data = json.data.children
+                        this.getnextpost = "Get next post"
+
+                        this.taptimes++;
+                        this.usernameDisplay = "/u/" + this.data[this.taptimes].data.author
+                        this.title = this.data[this.taptimes].data.title
+                        this.img = this.data[this.taptimes].data.thumbnail
+                    })
+            }
+        },
+
+        created() {
+            var welcometoast = Toast.makeText("Welcome!").show();
+            fetch("https://www.reddit.com/r/" + this.subreddit + ".json")
+                .then(response => response.json())
+                .then(json => {
+                    this.data = json.data.children
+                    this.getnextpost = "Get next post"
+
+                    this.taptimes++;
+                    this.usernameDisplay = "/u/" + this.data[this.taptimes].data.author
+                    this.title = this.data[this.taptimes].data.title
+                    this.img = this.data[this.taptimes].data.thumbnail
+                })
+        }
+
+    }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        /* text-align: center; */
+        font-size: 20;
+        color: #333333;
+    }
+
+    button {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    Image {
+        width: 200;
+        align-items: left;
+        text-align: left;
+    }
+
+    .bold {
+        font-weight: bold;
+    }
+</style>
